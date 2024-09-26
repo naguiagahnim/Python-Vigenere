a30e35c956b1f20f0b8c08c83476c1895564c904
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2024-09-26 10:32:10 +0200

    WIP fonction chiffrementVigenere

---
+++ module.py
@@ -0,0 +1,7 @@
+def chiffrementVigenere(texte, cle):
+    """Chiffre un texte avec la méthode de Vigenère
+    Entrée : texte (str) le texte à chiffrer, cle (str) la clé de chiffrement
+    Sortie : texteChiffre (str) le texte chiffré"""
+    texteChiffre = ""
+
+    return texteChiffre
